bdcf7fcf97b587afdfd2a85cf4ad3e6f411198a3
olaniyanp11 <olaniyanp11@gmail.com>
2023-08-18 01:23:40 +0100

    task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' Log parsing
+'''
+
+
+import sys
+
+
+def print_stats(file_size, status_codes):
+    '''Prints the stats
+    '''
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    count = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            file_size += int(data[-1])
+            if data[-2] in status_codes:
+                status_codes[data[-2]] += 1
+            if count % 10 == 0:
+                print_stats(file_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+    print_stats(file_size, status_codes)
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,18 @@
+### # 0x03. Log Parsing
+### Algorithm
+## Python
+###  By: Alexa Orrico, Software Engineer at Holberton School
+###  Weight: 1
+###  Project will start Aug 14, 2023 6:00 AM, must end by Aug 18, 2023 6:00 AM
+###  Checker was released at Aug 15, 2023 6:00 AM
+###  An auto review will be launched at the deadline
+## Requirements
+## General
+### Allowed editors: vi, vim, emacs
+### All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+### All your files should end with a new line
+### The first line of all your files should be exactly #!/usr/bin/python3
+### A README.md file, at the root of the folder of the project, is mandatory
+### Your code should use the PEP 8 style (version 1.7.x)
+### All your files must be executable
+### The length of your files will be tested using wcTasks
