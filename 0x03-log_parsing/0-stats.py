251befcd85b82153afbed807afb4717329ddf205
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2023-08-18 02:22:41 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-"""This module reads stdin line by line and computes metrics"""
+''' Log parsing
+'''
+
+
 import sys
 
-possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-lines_read = 0
-status_codes_map = {}
-total_file_size = 0
+
+def print_stats(file_size, status_codes):
+    '''Prints the stats
+    '''
+    print('File size: {:d}'.format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print('{:s}: {:d}'.format(key, value))
 
 
-def print_stats():
-    """prints out the statistics"""
-    print("File size: {}".format(total_file_size))
-    for status, count in sorted(status_codes_map.items()):
-        print("{}: {}".format(status, count))
-
-
-try:
-    for line in sys.stdin:
-        line_tokens = line.split()
-        try:
-            file_size = int(line_tokens[-1])
-            total_file_size += file_size
-            status_code = int(line_tokens[-2])
-            if status_code in possible_status_codes:
-                if status_code in status_codes_map:
-                    status_codes_map[status_code] += 1
-                else:
-                    status_codes_map[status_code] = 1
-        except ValueError:
-            pass
-        lines_read += 1
-        if lines_read % 10 == 0:
-            print_stats()
-
-    if (lines_read == 0) or (lines_read % 10 != 0):
-        print_stats()
-
-except (KeyboardInterrupt):
-    print_stats()
+if __name__ == '__main__':
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    file_size = 0
+    count = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            file_size += int(data[-1])
+            if data[-2] in status_codes:
+                status_codes[data[-2]] += 1
+            if count % 10 == 0:
+                print_stats(file_size, status_codes)
+    except (KeyboardInterrupt, EOFError):
+        print_stats(file_size, status_codes)
+    print_stats(file_size, status_codes)
+    
