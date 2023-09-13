6db1babeec72a68a3391c38604b190979f6307af
olaniyanp11 <olaniyanp11@gmail.com>
2023-09-13 01:36:41 +0000

    task 0 : pycodestyle compliant

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,14 +7,14 @@ def rotate_2d_matrix(matrix):
     Rotate a 2D matrix by 90 degrees clockwise.
     """
     length = len(matrix)
-    new_list =[]
+    new_list = []
     new_list = [[0 for _ in range(length)] for _ in range(length)]
 
-    for i in range(0,length):
+    for i in range(0, length):
         new_list[i] = matrix[i].copy()
 
     for i in range(length):
         num = length - 1
         for j in range(length):
             matrix[i][j] = new_list[num][i]
-            num -=1
+            num -= 1
