174fc43f8a2edc7aa7e2a50429823edf48b7f9c4
olaniyanp11 <olaniyanp11@gmail.com>
2023-09-27 17:40:23 +0100

    task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,6 +5,13 @@
 
 
 def island_perimeter(grid):
+    """
+    grid is a list of list of integers:
+    @0 represents water
+    @1 represents land
+    - Each cell is square, with a side length of 1
+    - Cells are connected horizontally/vertically (not diagonally)
+    """
     if not grid:
         return 0
 
@@ -14,17 +21,12 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-      
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-
                 if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-
     return perimeter
